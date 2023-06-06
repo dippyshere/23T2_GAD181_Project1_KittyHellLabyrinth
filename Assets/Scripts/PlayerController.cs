1d249020f96ebc42077f33f173fa671e2b528ec8
Jacob Asher - Home Desktop <31144233+Jasher1608@users.noreply.github.com>
2023-06-06 21:32:53 +1000

    Very simple player movement, reorganised folders

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Components
+    [Header("Components")]
+    private Rigidbody2D rb; // Rigidbody component
+    
+    // Movement variables
+    [Header("Movement Settings")] // Creates a header attribute in the inspector
+    [SerializeField] private float movementSpeed; // Controls how fast the player moves
+    private float vertical;
+    private float horizontal;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>(); // Assigns player's Rigidbody2D component
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Movement input
+        vertical = Input.GetAxisRaw("Vertical");
+        horizontal = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * movementSpeed, vertical * movementSpeed); // Move the player
     }
 }
