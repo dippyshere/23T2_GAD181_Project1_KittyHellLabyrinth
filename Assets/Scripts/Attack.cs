14a22b9f9df7d8289a944646ed25b9f5cdcde771
Jacob Asher - Home Desktop <31144233+Jasher1608@users.noreply.github.com>
2023-06-08 19:18:21 +1000

    Added "Attack" script, added sword placeholder sprite
    Attack script doesn't yet attack, it simply rotates the sword around the player object

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public Transform player;
+    public float rotationSpeed = 5f;
+    public float distanceFromPlayer = 2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        // Get the mouse position in world space
+        Vector2 mousePosition = GetMouseWorldPosition();
+
+        // Calculate the direction from the player to the mouse position
+        Vector2 directionToMouse = mousePosition - (Vector2)player.position;
+
+        // Calculate the target angle based on the direction to the mouse position
+        float targetAngle = Mathf.Atan2(directionToMouse.y, directionToMouse.x) * Mathf.Rad2Deg;
+
+        // Calculate the target position based on the mouse position and desired distance
+        Vector2 targetPosition = (Vector2)player.position + directionToMouse.normalized * distanceFromPlayer;
+
+        // Smoothly rotate the object towards the target angle
+        float rotationStep = rotationSpeed * Time.deltaTime;
+        float currentAngle = Mathf.MoveTowardsAngle(transform.eulerAngles.z, targetAngle, rotationStep);
+        transform.rotation = Quaternion.Euler(0f, 0f, currentAngle);
+
+        // Set the position of the object to the target position
+        transform.position = targetPosition;
+    }
+
+    Vector2 GetMouseWorldPosition()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = -Camera.main.transform.position.z;
+        return Camera.main.ScreenToWorldPoint(mousePosition);
+    }
+}
