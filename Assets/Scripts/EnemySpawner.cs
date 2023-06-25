c4b68586a09f063d0d0cc32278bcfa9c563813cb
Jacob Asher - Home Desktop <31144233+Jasher1608@users.noreply.github.com>
2023-06-25 16:42:13 +1000

    Changed enemy spawning to scale with time
    The formula for the enemy scaling is y = (2/3*x) + 2, where y is the number of enemies to spawn, and x is the number of waves that have already been spawned

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -6,14 +6,17 @@ public class EnemySpawner : MonoBehaviour
 {
 
     private Vector3 spawnLocation;
-    [SerializeField] private int enemiesToSpawn;
+    [SerializeField] private int enemiesToSpawnStart;
+    [SerializeField] private int enemiesToSpawn = 2;
 
     public GameObject enemyPrefab;
+    
+    private int wave = 2; // Starts at two as wave 1 is called in Start
 
     // Start is called before the first frame update
     void Start()
     { 
-        for (int i = 0; i < enemiesToSpawn; i++)
+        for (int i = 0; i < enemiesToSpawnStart; i++)
         {
             spawnLocation = new Vector3(Random.Range(-19, 24), Random.Range(-16, 14), 0);
             Instantiate(enemyPrefab, spawnLocation, Quaternion.identity);
@@ -25,9 +28,16 @@ public class EnemySpawner : MonoBehaviour
     {
         while (!GameObject.FindWithTag("Player").GetComponent<PlayerController>().isGameOver)
         {
-            spawnLocation = new Vector3(Random.Range(-19, 24), Random.Range(-16, 14), 0);
-            Instantiate(enemyPrefab, spawnLocation, Quaternion.identity);
-            yield return new WaitForSeconds(Random.Range(2, 10));
+            enemiesToSpawn = (int)Mathf.Round((2/3 * wave) + 2);
+            
+            for (int i = 0; i < enemiesToSpawn; i++)
+            {
+                spawnLocation = new Vector3(Random.Range(-19, 24), Random.Range(-16, 14), 0);
+                Instantiate(enemyPrefab, spawnLocation, Quaternion.identity);
+            }
+
+            wave++;
+            yield return new WaitForSeconds(Random.Range(5, 10));
         }
     }
 }
