c976a39a989913ea54484dfc515af3036e206825
Alex Hanson - Lab 2 102 <43489279+dippyshere@users.noreply.github.com>
2023-06-26 12:51:49 +1000

    replay button, disable submit after submission
    I rember!!!
    - adjust enemy hitboxes
    - adjust timer pickup
    - adjust camera limit
    - add replay button
    - disable submit button after submission
    - adjust other stuff i DONT rember 🤗

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI inputScore;
     [SerializeField] TMP_InputField inputName;
+    [SerializeField] Button submitButton;
 
     public UnityEvent<string, int> submitScoreEvent;
 
     public void SubmitScore()
     {
+        submitButton.interactable = false;
         Debug.LogFormat("'{0}'", inputScore.text);
         submitScoreEvent.Invoke(inputName.text, int.Parse(inputScore.text));
     }
